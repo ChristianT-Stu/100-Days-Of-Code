42426b27e8859f859e241b7f2b4c4fc0593cb6fc
ChristianT-Stu <69771935+ChristianT-Stu@users.noreply.github.com>
2023-05-03 11:21:51 -0400

    Initial commit

---
+++ Day 1/Homework and Practice/Exercise2.py
@@ -0,0 +1,27 @@
+#Instructions
+#Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+
+'''
+
+Example Output:
+
+Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n.
+
+'''
+
+#Fix the code below 👇
+
+'''print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")'''
+
+#Corrected Code:
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
---
+++ Day 1/Homework and Practice/Exercise3.py
@@ -0,0 +1,12 @@
+#Instructions
+
+#Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+'''
+Example Output
+
+Angela = 6
+
+'''
+
+def nameLength
