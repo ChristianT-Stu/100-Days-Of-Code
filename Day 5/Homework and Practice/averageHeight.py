c0f3478bb805787cc6e3f4b107dd8ff766f3b1c8
ChristianT-Stu <69771935+ChristianT-Stu@users.noreply.github.com>
2023-05-08 22:25:22 -0400

    Day 5 Complete

--- Day 5/Homework and Practice/averageHeight.py
+++ Day 5/Homework and Practice/averageHeight.py
@@ -23,7 +23,18 @@ Important You should not use the sum() or len() functions in your answer. You sh
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
+#print(student_heights)
 # 🚨 Don't change the code above 👆
+total_height = 0
+for height in student_heights:
+  total_height += height
 
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+#print(number_of_students)
 
-#Write your code below this row 👇
+averageHeight = total_height / number_of_students
+
+print(round(averageHeight))
+
